ebab6acbae2e6b433c9017e911e394799304fc25
kano1021 <muller1021@gmail.com>
2022-11-29 23:18:03 +0100

    edit model

--- devel/_setup_util.py
+++ devel/_setup_util.py
@@ -268,7 +268,7 @@ if __name__ == '__main__':
 
         if not args.local:
             # environment at generation time
-            CMAKE_PREFIX_PATH = r'/home/xinyi/carwitharm/devel;/opt/ros/melodic'.split(';')
+            CMAKE_PREFIX_PATH = r'/home/xinyi/my_panda/devel;/home/xinyi/manipulateur/devel;/home/xinyi/carwitharm/devel;/opt/ros/melodic'.split(';')
         else:
             # don't consider any other prefix path than this one
             CMAKE_PREFIX_PATH = []
